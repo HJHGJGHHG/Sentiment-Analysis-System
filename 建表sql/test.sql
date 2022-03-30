2b3d33636f4012b83749e9f93ad057d153415a9c
HJHGJGHHG <59946819+HJHGJGHHG@users.noreply.github.com>
2022-03-30 20:59:12 +0800

    更新需求分析报告

---
+++ 建表sql/test.sql
@@ -0,0 +1,10 @@
+USE 情感分析系统;
+/*
+INSERT INTO 个人信息表 VALUES(0, '顾客1', '123', '顾客');
+INSERT INTO 个人信息表 VALUES(1, '顾客2', '123', '顾客');
+INSERT INTO 个人信息表 VALUES(2, '管理员1', '123', '管理员');
+INSERT INTO 商品信息表 VALUES(100, '薯片');
+INSERT INTO 商品信息表 VALUES(101, '可乐');
+*/
+--INSERT INTO 评论信息表 VALUES(10000, 0, 100, '2022-3-30', '牛');
+SELECT * FROM 规则信息表;
